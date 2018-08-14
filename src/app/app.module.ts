405e6410400e1613e6449372806756b53880c94f
Bhagwatgita <rabindradahal2074@gmail.com>
2018-08-14 15:44:07 +0545

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent }
+  // ,
+  // { path: '', redirectTo: '', pathMatch: '' },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationbarComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
