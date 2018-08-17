8f85f3f90097416a9cf4e141e38e067c29f3145f
Bhagwatgita <rabindradahal2074@gmail.com>
2018-08-17 16:26:22 +0545

    Form email validation success

--- src/app/form-validation/form-validation.component.ts
+++ src/app/form-validation/form-validation.component.ts
@@ -8,14 +8,15 @@ import {FormControl, FormGroup, Validator, Validators} from '@angular/forms';
 })
 export class FormValidationComponent implements OnInit {
 
-  myform;
+  myform:FormGroup;
+  // submitted = false;
   constructor() { }
 
   ngOnInit() {
     this.myform = new FormGroup({
       first_name: new FormControl('', Validators.compose ([
           Validators.required,
-        Validators.minLength(3),
+        Validators.minLength(4),
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
      
@@ -34,16 +35,28 @@ export class FormValidationComponent implements OnInit {
     });
   }
 
-  textValidator(control) {
-    if (control.value.length < 3) {
-      return {'last_name': true};
-    }
+  // textValidator(control) {
+  //   if (control.value.length < 3) {
+  //     return {'last_name': true};
+  //   }
 
-  }
+  // }
   onSend = function (log) {
     console.log(log)
 
   }
+  get first_name() { return this.myform.get('first_name'); }
+  get email() { return this.myform.get('email'); }
+
+  // get f() { return this.myform.controls; }
+ 
+    // onSubmit() {
+    //     this.submitted = true;
+ 
+    //     // stop here if form is invalid
+    //     if (this.myform.invalid) {
+    //         return;
+    //     }
 
 
 }
